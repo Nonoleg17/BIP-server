e75eab2470e7dadc84db5ff4092cf6e5556b11d5
Nonoleg17 <Nonoleg17@mail.ru>
2021-04-20 14:23:47 +0300

    finish test

--- python_serv/views.py
+++ python_serv/views.py
@@ -5,6 +5,10 @@ from .models import PersonalData
 
 # Create your views here.
 def start_work(request):
-    t = PersonalData.objects.create(uid='213', name='123', surname='123',group='123')
-    t.save()
+    # t = PersonalData.objects.create(uid='213', name='123', surname='123',group='123')
+    # t.save()
+    if request.method == 'GET':
+        print(request.GET)
+    else:
+        print(request.POST)
     return HttpResponse('ok')
